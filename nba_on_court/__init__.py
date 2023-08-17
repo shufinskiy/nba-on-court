b5e8d5a0c207f0e18aeb5dc03fd97a923e3d1dc8
Vladislav Shufinskiy <hollywood.90210@mail.ru>
2023-08-18 00:11:33 +0300

    add get_nba_data function

--- nba_on_court/__init__.py
+++ nba_on_court/__init__.py
@@ -2,4 +2,5 @@ name = 'nba-on-court'
 __version__ = '0.1.8'
 
 from nba_on_court.nba_on_court import (players_on_court,
-                                       players_name)
+                                       players_name,
+                                       get_nba_data)
--- nba_on_court/nba_on_court.py
+++ nba_on_court/nba_on_court.py
@@ -1,6 +1,10 @@
 import warnings
+from pathlib import Path
+from itertools import product
+import urllib.request
+import tarfile
 from functools import lru_cache
-from typing import Optional, List, Dict, Union
+from typing import Optional, List, Dict, Union, Sequence
 
 import numpy as np
 import pandas as pd
@@ -186,7 +190,7 @@ def _cache_player_info(**kwargs: Dict[str, float]) -> commonallplayers.CommonAll
 
 
 def players_name(id_players: Union[pd.Series, np.ndarray, List],
-                       player_data: Optional[pd.DataFrame] = None, **kwargs: Dict[str, float]) -> List:
+                 player_data: Optional[pd.DataFrame] = None, **kwargs: Dict[str, float]) -> List:
     """Replacing player's PLAYER_ID with his full name
 
     Args:
@@ -220,3 +224,58 @@ def players_name(id_players: Union[pd.Series, np.ndarray, List],
         name_players.append(pl_id)
 
     return name_players
+
+def get_nba_data(path: Union[Path, str] = Path.cwd(),
+                 seasons: Union[Sequence, int] = range(1996, 2023),
+                 data: Union[Sequence, str] = ("datanba", "nbastats", "pbpstats", "shotdetail"),
+                 seasontype: str = 'rg',
+                 untar: bool = False) -> None:
+    """
+    Loading a nba play-by-play dataset from github repository https://github.com/shufinskiy/nba_data
+
+    Args:
+        seasons (Union[Sequence, int]): Sequence or integer of the year of start of season
+        data (Union[Sequence, str]): Sequence or string of data types to load
+        seasontype (str): Part of season: rg - Regular Season, po - Playoffs
+        untar (bool): Logical: do need to untar loaded archive
+
+    Returns:
+        None
+    """
+    if isinstance(path, str):
+        path = Path(path)
+    if isinstance(seasons, int):
+        seasons = (seasons,)
+    if isinstance(data, str):
+        data = (data,)
+
+    if seasontype == 'rg':
+        need_data = tuple(["_".join([data, str(season)]) for (data, season) in product(data, seasons)])
+    elif seasontype == 'po':
+        need_data = tuple(["_".join([data, seasontype, str(season)]) \
+                           for (data, seasontype, season) in product(data, (seasontype,), seasons)])
+    else:
+        need_data_rg = tuple(["_".join([data, str(season)]) for (data, season) in product(data, seasons)])
+        need_data_po = tuple(["_".join([data, seasontype, str(season)]) \
+                              for (data, seasontype, season) in product(data, ('po',), seasons)])
+        need_data = need_data_rg + need_data_po
+
+    with urllib.request.urlopen("https://raw.githubusercontent.com/shufinskiy/nba_data/main/list_data.txt") as f:
+        v = f.read().decode('utf-8').strip()
+
+    name_v = [string.split("=")[0] for string in v.split("\n")]
+    element_v = [string.split("=")[1] for string in v.split("\n")]
+
+    need_name = [name for name in name_v if name in need_data]
+    need_element = [element for (name, element) in zip(name_v, element_v) if name in need_data]
+
+    for i in range(len(need_name)):
+        t = urllib.request.urlopen(need_element[i])
+        with path.joinpath("".join([need_name[i], ".tar.xz"])).open(mode='wb') as f:
+        # with open("".join([need_name[i], ".tar.xz"]), 'wb') as f:
+            f.write(t.read())
+        if untar:
+            with tarfile.open(path.joinpath("".join([need_name[i], ".tar.xz"]))) as f:
+                f.extract("".join([need_name[i], ".csv"]), path)
+
+            path.joinpath("".join([need_name[i], ".tar.xz"])).unlink()
