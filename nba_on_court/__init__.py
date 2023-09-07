cf78c8e5e6aa2c1923ac68ca54705a8030cb5fe8
Vladislav Shufinskiy <hollywood.90210@mail.ru>
2023-09-07 14:29:40 +0300

    rename get_nba_data to load_nba_data

--- nba_on_court/__init__.py
+++ nba_on_court/__init__.py
@@ -3,6 +3,6 @@ __version__ = '0.1.8'
 
 from nba_on_court.nba_on_court import (players_on_court,
                                        players_name,
-                                       get_nba_data,
+                                       load_nba_data,
                                        left_join_nbastats,
                                        left_join_pbpstats)
