f9bbb9ef3dcdea86c17ac61ce03fe2ed5db7b6e7
Vladislav Shufinskiy <hollywood.90210@mail.ru>
2023-10-10 22:51:07 +0300

    upd name function in test folder

--- tests/test_nba_on_court.py
+++ tests/test_nba_on_court.py
@@ -7,7 +7,7 @@ from requests import ConnectionError
 
 
 def test_version():
-    assert __version__ == '0.1.8'
+    assert __version__ == '0.1.13'
 
 
 def test_players_start_quater(ten_players_period):
@@ -19,14 +19,14 @@ def test_players_start_quater(ten_players_period):
 
 
 def test_sort_players(ten_players_period):
-    unsort_pl = nba_on_court.players_in_quater(ten_players_period)
-    sort_pl = nba_on_court.sort_players(ten_players_period, unsort_pl)
+    unsort_pl = nba_on_court._players_in_quater(ten_players_period)
+    sort_pl = nba_on_court._sort_players(ten_players_period, unsort_pl)
     assert sort_pl == [202954, 1627737, 1629673, 203952, 1629672,
                        1627752, 1627747, 1629013, 201145, 1628386]
 
 
 def test_nine_players(nine_players_period):
-    player_id = nba_on_court.players_in_quater(nine_players_period)
+    player_id = nba_on_court._players_in_quater(nine_players_period)
     assert (202066 not in player_id) and (len(player_id) == 9)
 
 
@@ -37,7 +37,7 @@ def test_lost_internet(nine_players_period):
 
 
 def test_eleven_players(eleven_players_period):
-    player_id = nba_on_court.players_in_quater(eleven_players_period)
+    player_id = nba_on_court._players_in_quater(eleven_players_period)
     assert (202326 in player_id) and (len(player_id) == 11)
 
 
@@ -56,7 +56,7 @@ def test_cashe():
     ]
 )
 def test_fill_list_array(ten_players_period, all_id, shape):
-    df = nba_on_court.fill_columns(ten_players_period, all_id)
+    df = nba_on_court._fill_columns(ten_players_period, all_id)
     assert df.shape == shape
 
 
