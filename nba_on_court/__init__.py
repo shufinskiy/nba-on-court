c1e2e446772c6bb1214abfb9d0cfc2cf9c42cffe
Vladislav Shufinskiy <hollywood.90210@mail.ru>
2024-09-25 10:10:27 +0300

    upd version to 0.3.0

--- nba_on_court/__init__.py
+++ nba_on_court/__init__.py
@@ -1,8 +1,11 @@
 name = 'nba-on-court'
-__version__ = '0.2.1'
+__version__ = '0.3.0'
 
 from nba_on_court.nba_on_court import (players_on_court,
                                        players_name,
                                        load_nba_data,
                                        left_join_nbastats,
                                        left_join_pbpstats)
+
+from nba_on_court.hexdata import (calculate_hex_coords,
+                                  calculate_hexbins_from_shots)
